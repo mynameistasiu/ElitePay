426b96a30c1addd2819256cefd229ad82b5bd7a8
Tasiu Ismail <mynameistasiu@gmail.com>
2026-05-29 09:54:32 +0100

    Update buy-code.js

--- pages/buy-code.js
+++ pages/buy-code.js
@@ -6,7 +6,7 @@ import Layout from "../components/Layout";
 import { loadUser, saveTx } from "../utils/storage";
 
 const CODE_PRICE = 7000;
-const WA = "‪+2349016796843‬";
+const WA = "‪+2348133861975";
 const ONLINE_PAYMENT_URL = "https://checkout.korapay.com/pay/elitepayng";
 const ONLINE_RETURN_PATH = "/code?payment=online";
 
