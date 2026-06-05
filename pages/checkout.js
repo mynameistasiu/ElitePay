093998c8db19ce4cda791750a425ab83e67015cc
Tasiu Ismail <mynameistasiu@gmail.com>
2026-06-05 20:41:08 +0100

    Update checkout.js

--- pages/checkout.js
+++ pages/checkout.js
@@ -5,9 +5,9 @@ import { saveTx } from '../utils/storage';
 
 const CODE_PRICE = 7000;
 const DISPLAY_PRICE = 7000;
-const ACCOUNT_NUMBER = '6511699109';
+const ACCOUNT_NUMBER = '2082683908';
 const ACCOUNT_NAME = 'Abdulrahim Usman';
-const BANK_NAME = 'Moniepoint';
+const BANK_NAME = 'Kuda Bank';
 const WA = '‪+2347072369400';
 
 function CopyIcon({ size = 16 }) {
