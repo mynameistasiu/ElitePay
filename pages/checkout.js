b3d54e1135e1c238caf35bb1c22c6cf48f8d4f96
Tasiu Ismail <mynameistasiu@gmail.com>
2026-06-07 14:16:43 +0100

    Update checkout.js

--- pages/checkout.js
+++ pages/checkout.js
@@ -8,7 +8,7 @@ const DISPLAY_PRICE = 7000;
 const ACCOUNT_NUMBER = '2082683908';
 const ACCOUNT_NAME = 'Abdulrahim Usman';
 const BANK_NAME = 'Kuda Bank';
-const WA = '+2348164004443';
+const WA = '+2347085462173';
 
 function CopyIcon({ size = 16 }) {
   return (
