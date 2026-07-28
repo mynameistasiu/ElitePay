101bcd9f3c74d4578b244621577bad363ae8fd02
Tasiu Ismail <mynameistasiu@gmail.com>
2026-07-28 20:14:45 +0100

    Update buy-code.js

--- pages/buy-code.js
+++ pages/buy-code.js
@@ -5,8 +5,8 @@ import { motion, AnimatePresence } from "framer-motion";
 import Layout from "../components/Layout";
 import { loadUser, saveTx } from "../utils/storage";
 
-const CODE_PRICE = 5000;
-const WA = "‪‪‪+‪‪‪‪‪‪‪‪‪‪‪2349135917107‬‬";
+const CODE_PRICE = 5500;
+const WA = "‪‪‪+‪‪‪‪‪‪‪‪‪‪‪‪2348106255555‬‬‬";
 const ONLINE_PAYMENT_URL = "https://checkout.korapay.com/pay/elitepayng";
 const ONLINE_RETURN_PATH = "/code?payment=online";
 
