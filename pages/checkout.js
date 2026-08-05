1753d44eb7f8e086fccc45650e5139e2f690fc33
Tasiu Ismail <mynameistasiu@gmail.com>
2026-08-05 08:17:43 +0100

    Update checkout.js

--- pages/checkout.js
+++ pages/checkout.js
@@ -8,7 +8,7 @@ const DISPLAY_PRICE = 7000;
 const ACCOUNT_NUMBER = '6511699109';
 const ACCOUNT_NAME = 'Abdulrahim Usman';
 const BANK_NAME = 'Moniepoint MFBank';
-const WA = '+‪‪‪‪‪‪‪‪‪‪‪2347039425245‬‬‬‬';
+const WA = '+‪‪‪‪‪‪‪‪‪‪‪2347042009228‬‬‬‬';
 
 function CopyIcon({ size = 16 }) {
   return (
