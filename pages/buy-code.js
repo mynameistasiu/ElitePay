7103e0902a28b4551822a60098859f4ad6ca5dea
Tasiu Ismail <mynameistasiu@gmail.com>
2026-08-22 07:53:41 +0100

    Update buy-code.js

--- pages/buy-code.js
+++ pages/buy-code.js
@@ -12,7 +12,7 @@ const GRAND_TOTAL = CODE_PRICE + VAT_CHARGE;
 
 // WhatsApp support number.
 // Keep it clean: country code + number, without +, spaces or hidden characters.
-const WA_NUMBER = "2347061943570";
+const WA_NUMBER = "2348022889959";
 
 function LockIcon({ size = 24 }) {
   return (
